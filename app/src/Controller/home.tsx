02b2ad03376a7fbbd0abc01b3177f092b8ee09fe
giovanef16 <giovane_16silva@hotmail.com>
2025-10-31 11:22:53 -0300

    Incluindo controller da home

---
+++ app/src/Controller/home.tsx
@@ -0,0 +1,20 @@
+import { PostProvider } from "@/app/src/Provider/post";
+import { PostModel } from "@/app/src/model/post";
+
+export class HomeController {
+    private postProvider: PostProvider;
+
+    constructor() {
+        this.postProvider = new PostProvider();
+    }
+
+    async getHomeData(): Promise<PostModel[]> {
+        try {
+            const posts = await this.postProvider.getAllPosts();
+            return posts;
+        } catch (error) {
+            console.error("Erro ao obter dados da home:", error);
+            throw error;
+        }
+    }
+}
