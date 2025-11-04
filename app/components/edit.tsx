1bb5a957f332f0fb3dffb112291d0002e5b7ed93
giovanef16 <giovane_16silva@hotmail.com>
2025-11-04 08:46:59 -0300

    Removendo estilo inline e ajustando identação

---
+++ app/components/edit.tsx
@@ -0,0 +1,38 @@
+import { Post } from "@/app/src/Model/post";
+import { MaterialIcons } from "@expo/vector-icons";
+import { useRouter } from "expo-router";
+import React from "react";
+import { StyleSheet, Text, TouchableOpacity } from "react-native";
+
+const editPostButton = (post: Post) => {
+    const router = useRouter();
+
+    return (
+        <TouchableOpacity
+            onPress={() => router.push({ pathname: `view/post/edit/${post._id}`, params: { post: post } } as any)}
+            style={styles.button}
+        >
+            <Text style={styles.buttonText}>
+                <MaterialIcons name="edit" size={16} color="#ffffff" />
+                Editar Post
+            </Text>
+        </TouchableOpacity>
+    );
+}
+
+const styles = StyleSheet.create({
+    button: {
+        marginRight: 16,
+        backgroundColor: "#39d212ff",
+        paddingHorizontal: 12,
+        paddingVertical: 6,
+        borderRadius: 6,
+    },
+    buttonText: {
+        color: "#ffffff",
+        fontSize: 16,
+        fontWeight: "600",
+    },
+});
+
+export default editPostButton;
