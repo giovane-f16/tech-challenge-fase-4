8307c89e9e234b1a24516b9f1f770897230bd121
giovanef16 <giovane_16silva@hotmail.com>
2025-11-26 09:19:41 -0300

    Removendo arquivo keep e incluindo config-example

---
+++ app/src/Config/config-example.ts
@@ -0,0 +1,30 @@
+export class Config {
+    static getApiEndpoint(): string {
+        return "";
+    }
+
+    static getAuthToken(): string {
+        return "";
+    }
+
+    static getMongoDbUri(): string {
+        return "";
+    }
+
+    static getFirebaseConfig(): object {
+        return {
+            apiKey: "",
+            authDomain: "",
+            projectId: "",
+            storageBucket: "",
+            messagingSenderId: "",
+            appId: ""
+        };
+    }
+
+    static getSuperAdminEmail(): string {
+        return ""
+    }
+}
+
+export default Config;
