26de6053e4796543b6cfbf9a6ade6269c277a389
giovanef16 <giovane_16silva@hotmail.com>
2025-12-11 09:49:22 -0300

    Resolvendo warning

--- app/src/Config/firebase.ts
+++ app/src/Config/firebase.ts
@@ -1,11 +1,16 @@
 import Config from "@/app/src/Config/config";
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getReactNativePersistence, initializeAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = Config.getFirebaseConfig();
 const app = initializeApp(firebaseConfig);
 
-export const auth = getAuth(app);
+const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(ReactNativeAsyncStorage)
+});
+
 export const db = getFirestore(app);
+export { auth };
 export default app;
